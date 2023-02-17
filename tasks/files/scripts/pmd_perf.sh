f64f8eea74e9c8c4ce0511355bcacb83aeab721b
Pradipta Sahoo <psahoo@redhat.com>
2023-02-17 15:43:35 +0000

    v2.0

--- tasks/files/scripts/pmd_perf.sh
+++ tasks/files/scripts/pmd_perf.sh
@@ -1,5 +1,15 @@
+host=$(hostname -s)
+
 set -ux
 perf record -e raw_syscalls:sys_enter -e raw_syscalls:sys_exit \
-	-s --call-graph dwarf --per-thread -g -i \
+	-s --call-graph dwarf --per-thread -g -i -o /tmp/$host-perf1.data \
 	-t `ps -To tid,comm \`pidof ovs-vswitchd\` | grep pmd | awk '{$1=$1};1' | cut -d " " -f 1 | xargs | sed -e 's/ /,/g'` \
 	-- sleep 60
+
+sleep 10
+
+perf record -e syscalls:sys_enter_* -s --call-graph dwarf \
+	--per-thread -g -i -o /tmp/$host-perf2.data \
+	-t `ps -To tid,comm \`pidof ovs-vswitchd\` | grep pmd | awk '{$1=$1};1' | cut -d " " -f 1 | xargs | sed -e 's/ /,/g'` \
+	-- sleep 60
+

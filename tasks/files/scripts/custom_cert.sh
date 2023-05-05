764cb447fd3d43fffb51169caee2ddfee0597e07
Pradipta Sahoo <psahoo@redhat.com>
2023-05-05 12:46:11 +0000

    update

--- tasks/files/scripts/custom_cert.sh
+++ tasks/files/scripts/custom_cert.sh
@@ -1,8 +1,8 @@
 set -ux
 curl -O https://certs.corp.redhat.com/certs/2022-IT-Root-CA.pem
 curl -O https://certs.corp.redhat.com/certs/2015-IT-Root-CA.pem
-cp ~/2022-IT-Root-CA.pem /etc/pki/ca-trust/source/anchors/
-cp ~/2022-IT-Root-CA.pem /etc/pki/tls/certs/
-cp ~/2015-IT-Root-CA.pem /etc/pki/ca-trust/source/anchors/
-cp ~/2015-IT-Root-CA.pem /etc/pki/tls/certs/
+cp $PWD/2022-IT-Root-CA.pem /etc/pki/ca-trust/source/anchors/
+cp $PWD/2022-IT-Root-CA.pem /etc/pki/tls/certs/
+cp $PWD/2015-IT-Root-CA.pem /etc/pki/ca-trust/source/anchors/
+cp $PWD/2015-IT-Root-CA.pem /etc/pki/tls/certs/
 update-ca-trust
